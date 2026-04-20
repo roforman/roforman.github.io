21f8caa4b4a6c54ba0239ab77a1410f6a5b2f0f2
roforman <junsub98@gmail.com>
2026-04-20 11:55:56 +0900

    feat: 홈 회사개요 섹션 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import { useI18n } from '@/i18n/I18nContext'
 import styles from '@/styles/home.module.css'
-
-const GALLERY = [
-  { src: '/images/product-infusion-pump-prototype-01.png', alt: 'Infusion pump prototype test', capKey: 'home.cap1' },
-  { src: '/images/technology-wearable-assistive-robot-test-01.png', alt: 'Wearable assistive robot test', capKey: 'home.cap2' },
-  { src: '/images/technology-foldable-mobility-platform-prototype-01.png', alt: 'Foldable mobility platform prototype', capKey: 'home.cap3' },
-  { src: '/images/technology-humanoid-robot-demonstration-01.png', alt: 'Humanoid robot demonstration', capKey: 'home.cap4' },
-  { src: '/images/technology-stair-climbing-wheelchair-test-01.png', alt: 'Stair-climbing wheelchair test', capKey: 'home.cap5' },
-  { src: '/images/technology-wearable-exoskeleton-control-test-01.png', alt: 'Wearable exoskeleton control test', capKey: 'home.cap6' },
-]
 
 const RD_ITEMS = Array.from({ length: 13 }, (_, i) => `home.rdItem${i + 1}`)
 
@@ -60,20 +50,6 @@ export default function HomePage() {
         </div>
       </section>
 
-      <section className={styles.overview}>
-<div className={styles.overviewIntro}>
-          <h2>{t('home.overviewTitle')}</h2>
-          <p>{t('home.overviewBody')}</p>
-        </div>
-        <div className={`${styles.overviewGallery} media-grid`}>
-          {GALLERY.map((item) => (
-            <figure key={item.src} className="media-card">
-              <Image src={item.src} alt={item.alt} width={400} height={300} />
-              <figcaption>{t(item.capKey)}</figcaption>
-            </figure>
-          ))}
-        </div>
-      </section>
     </>
   )
 }
