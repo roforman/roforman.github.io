03e211587b43b8a4e4d6fdbb0ffb191864e46505
roforman <junsub98@gmail.com>
2026-04-20 11:58:15 +0900

    feat: 홈 페이지에 C-arm 및 외골격 기술 카드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 import { useI18n } from '@/i18n/I18nContext'
 import styles from '@/styles/home.module.css'
+import companyStyles from '@/styles/company.module.css'
 
 const RD_ITEMS = Array.from({ length: 13 }, (_, i) => `home.rdItem${i + 1}`)
 
@@ -50,6 +52,97 @@ export default function HomePage() {
         </div>
       </section>
 
+      {/* C-arm 기술 카드 */}
+      <section className="page-card">
+        <h2 className={companyStyles.techItemTitle}>{t('company.carmTitle')}</h2>
+        <ul className={companyStyles.techList}>
+          <li>
+            {t('company.carmProblemTitle')}
+            <ul>
+              <li>{t('company.carmProblem1')}</li>
+              <li>{t('company.carmProblem2')}</li>
+            </ul>
+          </li>
+          <li>
+            {t('company.carmSolutionTitle')}
+            <ul>
+              <li>{t('company.carmSolution1')}</li>
+              <li>{t('company.carmSolution2')}</li>
+            </ul>
+          </li>
+        </ul>
+        <div className={companyStyles.techMediaGrid}>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.techImgWrap}>
+              <Image src="/images/carm-1.png" alt="C-arm overview" fill style={{ objectFit: 'contain' }} />
+            </div>
+          </figure>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.techImgWrap}>
+              <Image src="/images/carm-2.jpg" alt="C-arm platform" fill style={{ objectFit: 'contain' }} />
+            </div>
+            <figcaption>{t('company.carmImgCaption')}</figcaption>
+          </figure>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.techImgWrap}>
+              <Image src="/images/carm-3.png" alt="C-arm diagram" fill style={{ objectFit: 'contain' }} />
+            </div>
+          </figure>
+        </div>
+      </section>
+
+      {/* Exoskeletal 기술 카드 */}
+      <section className="page-card">
+        <h2 className={companyStyles.techItemTitle}>{t('company.exoTitle')}</h2>
+        <ul className={companyStyles.techList}>
+          <li>
+            {t('company.exoDeviceTitle')}
+            <ul>
+              <li>{t('company.exoDevice1')}</li>
+              <li>{t('company.exoDevice2')}</li>
+              <li>{t('company.exoDevice3')}</li>
+            </ul>
+          </li>
+          <li>
+            {t('company.exoSystemTitle')}
+            <ul>
+              <li>{t('company.exoSystem1')}</li>
+              <li>{t('company.exoSystem2')}</li>
+              <li>{t('company.exoSystem3')}</li>
+            </ul>
+          </li>
+        </ul>
+        <div className={companyStyles.techMediaGrid}>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.techImgWrap}>
+              <Image src="/images/exoskeletal-1.png" alt="Exoskeletal experimental setup" fill style={{ objectFit: 'contain' }} />
+            </div>
+            <figcaption>{t('company.exoImgCaption')}</figcaption>
+          </figure>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.videoWrapper}>
+              <iframe
+                src="https://www.youtube.com/embed/ueK4BHaNQpk"
+                title="Master device"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+            <figcaption>{t('company.exoVideo1Caption')}</figcaption>
+          </figure>
+          <figure className={companyStyles.techFigure}>
+            <div className={companyStyles.videoWrapper}>
+              <iframe
+                src="https://www.youtube.com/embed/6sHSHove0Pg"
+                title="Teleoperation demonstration"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+            <figcaption>{t('company.exoVideo2Caption')}</figcaption>
+          </figure>
+        </div>
+      </section>
     </>
   )
 }
